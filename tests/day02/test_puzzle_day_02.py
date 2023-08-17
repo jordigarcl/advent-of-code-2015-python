81bc0e2c1c7faeee3d1ee54e1cf08210276394b2
jordigarcl <jgarcialisson@gmail.com>
2023-08-17 17:53:45 +0200

    day02-puzzle2

--- tests/day02/test_puzzle_day_02.py
+++ tests/day02/test_puzzle_day_02.py
@@ -13,6 +13,18 @@ class PuzzleDay02:
 
         return sum(list(inner(puzzle_input)))
 
+    @staticmethod
+    def solve_puzzle_2(puzzle_input):
+        def inner(puzzle_input):
+            for line in puzzle_input.splitlines():
+                length, width, height = [int(number) for number in line.split('x')]
+
+                wrapping_ribbon = min(2 * (length + width), 2 * (width + height), 2 * (height + length))
+                bow_ribbon = length * width * height
+                yield wrapping_ribbon + bow_ribbon
+
+        return sum(list(inner(puzzle_input)))
+
 
 class TestPuzzleDay02(unittest.TestCase):
 
@@ -25,9 +37,22 @@ class TestPuzzleDay02(unittest.TestCase):
         self.assertEqual(puzzle_solution, PuzzleDay02.solve_puzzle_1(puzzle_input))
 
     def test_puzzle1(self):
-        puzzle_input = open("input_puzzle1.txt", "r").read()
-        puzzle_answer = 232
+        puzzle_input = open("input_puzzle.txt", "r").read()
+        puzzle_answer = 1588178
         self.assertEqual(puzzle_answer, PuzzleDay02.solve_puzzle_1(puzzle_input))
+
+    @parameterized.expand([
+        ("2x3x4", 34),
+        ("1x1x10", 14)
+    ])
+    def test_puzzle2_examples(self, puzzle_input, puzzle_solution):
+        self.assertEqual(puzzle_solution, PuzzleDay02.solve_puzzle_2(puzzle_input))
+
+    def test_puzzle2(self):
+        puzzle_input = open("input_puzzle.txt", "r").read()
+        puzzle_answer = 3783758
+        self.assertEqual(puzzle_answer, PuzzleDay02.solve_puzzle_2(puzzle_input))
+
 
 
 if __name__ == '__main__':
